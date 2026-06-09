3aae0d088bff5246c32c95c7f5ab889ca523dfe1
MaxAutomize <max.rippley22@outlook.com>
2026-06-09 09:59:46 -0600

    Move main title to top as wide script arc

--- app/page.js
+++ app/page.js
@@ -5,15 +5,18 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="menu-shell">
-      <h1 className="menu-title" aria-label="LittleRip">
-        {'LittleRip'.split('').map((letter, i) => {
-          const center = 4
-          const distance = i - center
-          const y = -42 + (distance * distance) * 2.6
-          const rotate = distance * 2.2
-          return <span key={i} style={{ '--y': `${y}px`, '--r': `${rotate}deg` }}>{letter}</span>
-        })}
-      </h1>
+      <div className="menu-title-wrap" aria-label="LittleRip">
+        <svg className="menu-title-svg" viewBox="0 0 1200 260" preserveAspectRatio="none" aria-hidden="true">
+          <defs>
+            <path id="little-rip-title-arc" d="M 20 230 C 210 16, 990 16, 1180 230" />
+          </defs>
+          <text className="menu-title-svg-text">
+            <textPath href="#little-rip-title-arc" startOffset="50%" textAnchor="middle">
+              LittleRip
+            </textPath>
+          </text>
+        </svg>
+      </div>
 
       <nav className="menu-options">
         <Link href="/chat" className="menu-card">Chat</Link>
