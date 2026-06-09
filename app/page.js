1179ab32ce35fde82e06a86d63d852dd3c94d8f8
MaxAutomize <max.rippley22@outlook.com>
2026-06-09 10:17:02 -0600

    Flat centered title, no arc, higher up, Pinyon Script

--- app/page.js
+++ app/page.js
@@ -5,18 +5,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="menu-shell">
-      <div className="menu-title-wrap" aria-label="LittleRip">
-        <svg className="menu-title-svg" viewBox="0 0 1400 220" preserveAspectRatio="xMidYMid meet" aria-hidden="true">
-          <defs>
-            <path id="little-rip-arc" d="M 20 210 C 200 2, 1200 2, 1380 210" />
-          </defs>
-          <text className="svg-title-text">
-            <textPath href="#little-rip-arc" startOffset="50%" textAnchor="middle">
-              LittleRip
-            </textPath>
-          </text>
-        </svg>
-      </div>
+      <h1 className="menu-title">LittleRip</h1>
 
       <nav className="menu-options">
         <Link href="/chat" className="menu-card">Chat</Link>
