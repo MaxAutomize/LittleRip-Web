7c8e2c0d71c185d690f53b8c60b8b49978e5376c
MaxAutomize <max.rippley22@outlook.com>
2026-06-09 10:03:44 -0600

    Fix title: bigger thinner arc spanning edge to edge

--- app/page.js
+++ app/page.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <main className="menu-shell">
       <div className="menu-title-wrap" aria-label="LittleRip">
-        <svg className="menu-title-svg" viewBox="0 0 1200 260" preserveAspectRatio="none" aria-hidden="true">
+        <svg className="menu-title-svg" viewBox="0 0 1400 280" preserveAspectRatio="xMidYMid meet" aria-hidden="true">
           <defs>
-            <path id="little-rip-title-arc" d="M 20 230 C 210 16, 990 16, 1180 230" />
+            <path id="little-rip-arc" d="M 30 260 C 260 30, 1140 30, 1370 260" />
           </defs>
-          <text className="menu-title-svg-text">
-            <textPath href="#little-rip-title-arc" startOffset="50%" textAnchor="middle">
+          <text className="svg-title-text">
+            <textPath href="#little-rip-arc" startOffset="50%" textAnchor="middle">
               LittleRip
             </textPath>
           </text>
